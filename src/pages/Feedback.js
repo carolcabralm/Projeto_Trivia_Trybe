e24ca937dd8c9a2f9cf49bcbed45f1fe1a92bcf4
Rafael <rafaelratier@hotmail.com>
2022-03-15 15:23:16 -0300

    Requisitos 15 e 16 completos

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -16,6 +16,16 @@ class Feedback extends React.Component {
     }
   }
 
+  handleClickPlayAgain = () => {
+    const { history } = this.props;
+    history.push('./');
+  }
+
+  handleClickRanking = () => {
+    const { history } = this.props;
+    history.push('./ranking');
+  }
+
   render() {
     const { wellDone } = this.state;
     const { correctAnswers, finalScore } = this.props;
@@ -32,6 +42,22 @@ class Feedback extends React.Component {
         <p data-testid="feedback-total-score">{ finalScore }</p>
         Número de respostas corretas:
         <p data-testid="feedback-total-question">{ correctAnswers }</p>
+        <button
+          id="playAgain"
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.handleClickPlayAgain }
+        >
+          Play again
+        </button>
+        <button
+          id="rankingBtn"
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.handleClickRanking }
+        >
+          Ranking
+        </button>
       </>
     );
   }
@@ -40,6 +66,7 @@ class Feedback extends React.Component {
 Feedback.propTypes = {
   correctAnswers: PropTypes.number.isRequired,
   finalScore: PropTypes.number.isRequired,
+  history: PropTypes.instanceOf(Object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
---
+++ src/pages/Ranking.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="ranking-title">Ranking</p>
+      </div>
+    );
+  }
+}
+
+export default Ranking;

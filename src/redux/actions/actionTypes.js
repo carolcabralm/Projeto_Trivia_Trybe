87847ce06ee637b0bfaf0332a6348f3cb314b07e
Rafael <rafaelratier@hotmail.com>
2022-03-14 22:43:30 -0300

    Requisito 9 completo

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,3 +1,4 @@
 export const LOGIN = 'LOGIN';
 export const TOKEN = 'TOKEN';
 export const QUIZ = 'QUIZ';
+export const ADD_SCORE = 'ADD_SCORE';
---
+++ src/redux/reducers/reducerScore.js
@@ -0,0 +1,17 @@
+// import { ADD_SCORE } from '../actions/actionTypes';
+
+// const INICIAL_STATE = {
+//   score: 0,
+// };
+// function reducerScore(state = INICIAL_STATE, action) {
+//   switch (action.type) {
+//   case ADD_SCORE:
+//     return {
+//       ...state,
+//       score: state.score + action.payload,
+//     };
+//   default:
+//     return state;
+//   }
+// }
+// export default reducerScore;

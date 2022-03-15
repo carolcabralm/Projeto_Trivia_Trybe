0829fa0883eb5c99dcf0ca927280a431125dc27b
carolcabralm <carolcabralm@msn.com>
2022-03-15 08:24:31 -0300

    Group 14 Requisito 14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -18,6 +18,7 @@ class Feedback extends React.Component {
 
   render() {
     const { wellDone } = this.state;
+    const { correctAnswers, finalScore } = this.props;
     return (
       <>
         <Header />
@@ -27,6 +28,10 @@ class Feedback extends React.Component {
             ? <p data-testid="feedback-text">Well Done!</p>
             : <p data-testid="feedback-text">Could be better...</p>
         }
+        Placar Final:
+        <p data-testid="feedback-total-score">{ finalScore }</p>
+        Número de respostas corretas:
+        <p data-testid="feedback-total-question">{ correctAnswers }</p>
       </>
     );
   }
@@ -34,10 +39,12 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   correctAnswers: PropTypes.number.isRequired,
+  finalScore: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   correctAnswers: state.player.assertions,
+  finalScore: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
